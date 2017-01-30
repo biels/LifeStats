8c85a3c6310c49f33bb04468a78b5fe81340fd90
Biel Simon Rojas <bielsimon2@gmail.com>
2017-01-30 11:22:03 +0100

    Initial server commit

---
+++ ls-server/src/main/java/com/biel/lifestats/LsServerApplication.java
@@ -0,0 +1,12 @@
+package com.biel.lifestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LsServerApplication.class, args);
+	}
+}
